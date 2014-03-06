a2ec6534c452a8ecfcaf29e5536e8d5508447973
George Redinger <george@georgeredinger.com>
2014-03-06 13:38:25 -0800

    adxl345 FiFo trigger with fifo read

--- ADXL375/ADXL375.ino
+++ ADXL375/ADXL375.ino
@@ -15,20 +15,20 @@ byte buff[6] ;    //6 bytes buffer for saving data read from the device
 
 
 void writeTo(int device, byte address, byte val) {
-   Wire.beginTransmission(device); //start transmission to device 
-   Wire.write(address);        // send register address
-   Wire.write(val);        // send value to write
-   Wire.endTransmission(); //end transmission
+  Wire.beginTransmission(device); //start transmission to device 
+  Wire.write(address);        // send register address
+  Wire.write(val);        // send value to write
+  Wire.endTransmission(); //end transmission
 }
 
 void readFrom(int device, byte address, int num, byte buff[]) {
   Wire.beginTransmission(device); //start transmission to device 
   Wire.write(address);        //sends address to read from
   Wire.endTransmission(); //end transmission
-  
-  Wire.beginTransmission(device); //start transmission to device (initiate again)
+
+    Wire.beginTransmission(device); //start transmission to device (initiate again)
   Wire.requestFrom(device, num);    // request 6 bytes from device
-  
+
   int i = 0;
   while(Wire.available())    //device may send less than requested (abnormal)
   { 
@@ -41,77 +41,92 @@ void readFrom(int device, byte address, int num, byte buff[]) {
 void setup(){ 
   pinMode(led,OUTPUT);
   pinMode(interrupt_pin, INPUT);
- // digitalWrite(interrupt_pin, HIGH);    // pullup
+  // digitalWrite(interrupt_pin, HIGH);    // pullup
   attachInterrupt(0, biff, RISING);
- 
- 
+
+
 
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
- // Serial.println("Begin");
+  // Serial.println("Begin");
   //Turning on the ADXL345
   //delay(60);
   writeTo(DEVICE, 0x2D, 0);      
   writeTo(DEVICE, 0x2D, 16);
-  
+
   //1. Write 0x28 to Register 0x1D; set shock threshold to 31.2 g.
   writeTo(DEVICE,0x1D,0x28); // 2,5 Gs for an ADXL345,(OX28/0xFF)*FSR
-  
-//2. Write 0x50 to Register 0x21; set shock duration to 50 ms.
- writeTo(DEVICE,0x21,0x50);
-//3. Write 0x20 to Register 0x22; set latency to 40 ms.
-//
- writeTo(DEVICE,0x22,0x20);
-//4. Write 0xF0 to Register 0x23; set shock window to 300 ms.
- writeTo(DEVICE,0x23,0xF0);
-//5. Write 0x07 to Register 0x2A to enable X-, Y-, and Z-axes
-// participation in shock detection.
- writeTo(DEVICE,0x2A,0x07);
-//6. Write 0x0F to Register 0x2C to set output data rate to
-//  3200 Hz.
- writeTo(DEVICE,0x2c,0X0F);
-//7. Write 0x40/0x20 to Register 0x2E to enable single shock or
-//  double shock, respectively.
- writeTo(DEVICE,0x2E,0x40);
-//8. Write 0x40/0x20 to Register 0x2F to assign single shock or
-//  double shock interrupt, respectively, to the INT2 pin.
- writeTo(DEVICE,0x2F,0X40);
-//9.  Write 0xEA to Register 0x38 to enable triggered mode
-//FIFO. If an interrupt is detected on the INT2 pin, the FIFO
-//records the trigger event acceleration with 10 samples
-//retained from before the trigger event.
- writeTo(DEVICE,0x38,0XEA);
-//10. Write 0x08 to Register 0x2D to start the measurement.
-//  It is recommended that the POWER_CTL register be
-//  configured last.
- writeTo(DEVICE,0x2D,0x08);
+
+  //2. Write 0x50 to Register 0x21; set shock duration to 50 ms.
+  writeTo(DEVICE,0x21,0x50);
+  //3. Write 0x20 to Register 0x22; set latency to 40 ms.
+  //
+  writeTo(DEVICE,0x22,0x20);
+  //4. Write 0xF0 to Register 0x23; set shock window to 300 ms.
+  writeTo(DEVICE,0x23,0xF0);
+  //5. Write 0x07 to Register 0x2A to enable X-, Y-, and Z-axes
+  // participation in shock detection.
+  writeTo(DEVICE,0x2A,0x07);
+  //6. Write 0x0F to Register 0x2C to set output data rate to
+  //  3200 Hz.
+  writeTo(DEVICE,0x2c,0X0F);
+  //7. Write 0x40/0x20 to Register 0x2E to enable single shock or
+  //  double shock, respectively.
+  writeTo(DEVICE,0x2E,0x40);
+  //8. Write 0x40/0x20 to Register 0x2F to assign single shock or
+  //  double shock interrupt, respectively, to the INT2 pin.
+  writeTo(DEVICE,0x2F,0X40);
+  //9.  Write 0xEA to Register 0x38 to enable triggered mode
+  //FIFO. If an interrupt is detected on the INT2 pin, the FIFO
+  //records the trigger event acceleration with 10 samples
+  //retained from before the trigger event.
+  writeTo(DEVICE,0x38,0XEA);
+  //10. Write 0x08 to Register 0x2D to start the measurement.
+  //  It is recommended that the POWER_CTL register be
+  //  configured last.
+  writeTo(DEVICE,0x2D,0x08);
 }
 //int16_t ax, ay, az;
 volatile  bool bam=false;
 //
 void biff(){
-    bam=true;
-        digitalWrite(led,!digitalRead(led));
+  bam=true;
+  digitalWrite(led,!digitalRead(led));
 
- // if((unsigned)buff[0] & 0x40) {
+  // if((unsigned)buff[0] & 0x40) {
   //   bams++;
   //   bam=true;
-   //  Serial.println("Oow");
- // }
- 
+  //  Serial.println("Oow");
+  // }
+
 }
 
 
-void loop(){
-//  enterSleep();
-//      accel.getAcceleration(&ax, &ay, &az);
+char str[64];                      //string buffer to transform data before sending it to the serial port
 
-if(bam) {
+void loop(){
+  int x, y, z;
+  int i=0;
+  if(bam) {
     bam=false;
-     readFrom(DEVICE,0X30,1,buff);
+    delay(10);
+    readFrom(DEVICE,0X30,1,buff);
+    Serial.println("Bamo-------------------");
+    for(i=0;i<32;i++){
+      readFrom(DEVICE, 0X32, TO_READ, buff); //read the acceleration data from the ADXL345
+      x = (((int)buff[1]) << 8) | buff[0];   
+      y = (((int)buff[3])<< 8) | buff[2];
+      z = (((int)buff[5]) << 8) | buff[4];
+
+      sprintf(str, "%d, %d ,%d ,%d", i,x, y, z);  
+      Serial.print(str);
+      Serial.write(10);
+
+    }
 
   }
 }
+
 
 
 
