5fd0578d28d9270804c8a35bc33d6d2a7c617b21
George Redinger <george@georgeredinger.com>
2014-03-06 13:11:58 -0800

    basic FiFo interrupt

--- ADXL375/ADXL375.ino
+++ ADXL375/ADXL375.ino
@@ -1,211 +1,119 @@
 //Add the SPI library so we can communicate with the ADXL345 sensor
 
-#include <SPI.h>
-#include <avr/sleep.h>
-#include <avr/power.h>
-#include <avr/wdt.h>
+//#include <avr/sleep.h>
+//#include <avr/power.h>
+//#include <avr/wdt.h>
+#include "Pins.h"
+#include <Wire.h>
 
-volatile int toggle = 0;
-const int interrupt_pin = 2;
-const int led_pin = 9;
-#define LED_PIN (9)
-volatile int wakeups=0;
-int CS=10;
-volatile boolean kapow=false;
-#define DEVID 0
-char POWER_CTL = 0x2D;	//Power Control Register
-char DATA_FORMAT = 0x31;
-char DATAX0 = 0x32;	//X-Axis Data 0
-char DATAX1 = 0x33;	//X-Axis Data 1
-char DATAY0 = 0x34;	//Y-Axis Data 0
-char DATAY1 = 0x35;	//Y-Axis Data 1
-char DATAZ0 = 0x36;	//Z-Axis Data 0
-char DATAZ1 = 0x37;	//Z-Axis Data 1
+#define DEVICE (0x53)    //ADXL345 device address
+#define TO_READ (6)        //num of bytes we are going to read each time (two bytes for each axis)
 
 
-//ADXL345 Register Addresses
-#define	DEVID		0x00	//Device ID Register
-#define THRESH_TAP	0x1D	//Tap Threshold
-#define	OFSX		0x1E	//X-axis offset
-#define	OFSY		0x1F	//Y-axis offset
-#define	OFSZ		0x20	//Z-axis offset
-#define	DURATION	0x21	//Tap Duration
-#define	LATENT		0x22	//Tap latency
-#define	WINDOW		0x23	//Tap window
-#define	THRESH_ACT	0x24	//Activity Threshold
-#define	THRESH_INACT	0x25	//Inactivity Threshold
-#define	TIME_INACT	0x26	//Inactivity Time
-#define	ACT_INACT_CTL	0x27	//Axis enable control for activity and inactivity detection
-#define	THRESH_FF	0x28	//free-fall threshold
-#define	TIME_FF		0x29	//Free-Fall Time
-#define	TAP_AXES	0x2A	//Axis control for tap/double tap
-#define ACT_TAP_STATUS	0x2B	//Source of tap/double tap
-#define	BW_RATE		0x2C	//Data rate and power mode control
-#define POWER_CTL	0x2D	//Power Control Register
-#define	INT_ENABLE	0x2E	//Interrupt Enable Control
-#define	INT_MAP		0x2F	//Interrupt Mapping Control
-#define	INT_SOURCE	0x30	//Source of interrupts
-#define	DATA_FORMAT	0x31	//Data format control
-#define DATAX0		0x32	//X-Axis Data 0
-#define DATAX1		0x33	//X-Axis Data 1
-#define DATAY0		0x34	//Y-Axis Data 0
-#define DATAY1		0x35	//Y-Axis Data 1
-#define DATAZ0		0x36	//Z-Axis Data 0
-#define DATAZ1		0x37	//Z-Axis Data 1
-#define	FIFO_CTL	0x38	//FIFO control
-#define	FIFO_STATUS	0x39	//FIFO status
-
-//This buffer will hold values read from the ADXL345 registers.
-char values[10];
-char output[20];
-//These variables will be used to hold the x,y and z axis accelerometer values.
-int x,y,z;
-double xg, yg, zg;
-char tapType=0;
-
-ISR(WDT_vect){
-   if(wakeups % 4 == 0)
-      digitalWrite(LED_PIN, HIGH);
-     else
-      digitalWrite(LED_PIN,LOW);
- }
+byte buff[6] ;    //6 bytes buffer for saving data read from the device
 
 
 
-void enterSleep(void){
-  delay(100);
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
- //  set_sleep_mode(SLEEP_MODE_PWR_SAVE);   
-  sleep_enable();
-   sleep_mode();
+void writeTo(int device, byte address, byte val) {
+   Wire.beginTransmission(device); //start transmission to device 
+   Wire.write(address);        // send register address
+   Wire.write(val);        // send value to write
+   Wire.endTransmission(); //end transmission
+}
+
+void readFrom(int device, byte address, int num, byte buff[]) {
+  Wire.beginTransmission(device); //start transmission to device 
+  Wire.write(address);        //sends address to read from
+  Wire.endTransmission(); //end transmission
   
-  /* The program will continue from here. */
+  Wire.beginTransmission(device); //start transmission to device (initiate again)
+  Wire.requestFrom(device, num);    // request 6 bytes from device
   
-  /* First thing to do is disable sleep. */
-  sleep_disable(); 
-  power_all_enable();
+  int i = 0;
+  while(Wire.available())    //device may send less than requested (abnormal)
+  { 
+    buff[i] = Wire.read(); // receive a byte
+    i++;
+  }
+  Wire.endTransmission(); //end transmission
 }
 
 void setup(){ 
-   pinMode(led_pin,OUTPUT);
+  pinMode(led,OUTPUT);
   pinMode(interrupt_pin, INPUT);
-//  attachInterrupt(0, pin2Interrupt, RISING);
-  
-  Serial.println("Initialisation complete.");
-      digitalWrite(led_pin, HIGH);
+ // digitalWrite(interrupt_pin, HIGH);    // pullup
+  attachInterrupt(0, biff, RISING);
+ 
+ 
 
-  //Initiate an SPI communication instance.
-  SPI.begin();
-  SPI.setClockDivider(SPI_CLOCK_DIV32);
-  //Configure the SPI connection for the ADXL345.
-  SPI.setDataMode(SPI_MODE3);
-  //Create a serial connection to display the data on the terminal.
-  Serial.begin(9600);
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+ // Serial.println("Begin");
+  //Turning on the ADXL345
+  //delay(60);
+  writeTo(DEVICE, 0x2D, 0);      
+  writeTo(DEVICE, 0x2D, 16);
   
-  //Set up the Chip Select pin to be an output from the Arduino.
-  pinMode(CS, OUTPUT);
-  //Before communication starts, the Chip Select pin needs to be set high.
-  digitalWrite(CS, HIGH);
+  //1. Write 0x28 to Register 0x1D; set shock threshold to 31.2 g.
+  writeTo(DEVICE,0x1D,0x28); // 2,5 Gs for an ADXL345,(OX28/0xFF)*FSR
   
-//  writeRegister(DATA_FORMAT, 0); //full range?
-  //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
-//  writeRegister(POWER_CTL, 0x08);  //Measurement mode  
-  delay(100);
-   /* Clear the reset flag. */
-  MCUSR &= ~(1<<WDRF);
-  
-  /* In order to change WDE or the prescaler, we need to
-   * set WDCE (This will allow updates for 4 clock cycles).
-   */
-  WDTCSR |= (1<<WDCE) | (1<<WDE);
-
-  /* set new watchdog timeout prescaler value */
-  WDTCSR = 0<<WDP3 | 1<< WDP2 | 0<<WDP1| 0<<WDP0  ; /* .25 seconds */
-  
-  /* Enable the WD interrupt (note no reset). */
-  WDTCSR |= _BV(WDIE);
-  
-  Serial.println("Initialisation complete.");
-  delay(100); //Allow for serial print to complete.
-    attachInterrupt(0, tap, RISING);
-  //Put the ADXL345 into +/- 4G range by writing the value 0x01 to the DATA_FORMAT register.
-  writeRegister(DATA_FORMAT, 0x01);
-
-  //Send the Tap and Double Tap Interrupts to INT1 pin
-  writeRegister(INT_MAP, 0x9F);
-  //Look for taps on the Z axis only.
-  writeRegister(TAP_AXES, 0x01);
-  //writeRegister(THRESH_TAP, 0x7f); // 8G or 1/2 of full scale range
-    writeRegister(THRESH_TAP, 0xff); // 16G  full scale range
-
-  //Set the Tap Duration that must be reached
-  writeRegister(DURATION, 0x10);
-  
-  //100ms Latency before the second tap can occur.
-  writeRegister(LATENT, 0x50);
-  writeRegister(WINDOW, 0xFF);
-  
-  //Enable the Single and Double Taps.
-  writeRegister(INT_ENABLE, 0xE0);  
-  
-  //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
-  writeRegister(POWER_CTL, 0x08);  //Measurement mode
-  readRegister(INT_SOURCE, 1, values); //Clear the interrupts from the INT_SOURCE register.
+//2. Write 0x50 to Register 0x21; set shock duration to 50 ms.
+ writeTo(DEVICE,0x21,0x50);
+//3. Write 0x20 to Register 0x22; set latency to 40 ms.
+//
+ writeTo(DEVICE,0x22,0x20);
+//4. Write 0xF0 to Register 0x23; set shock window to 300 ms.
+ writeTo(DEVICE,0x23,0xF0);
+//5. Write 0x07 to Register 0x2A to enable X-, Y-, and Z-axes
+// participation in shock detection.
+ writeTo(DEVICE,0x2A,0x07);
+//6. Write 0x0F to Register 0x2C to set output data rate to
+//  3200 Hz.
+ writeTo(DEVICE,0x2c,0X0F);
+//7. Write 0x40/0x20 to Register 0x2E to enable single shock or
+//  double shock, respectively.
+ writeTo(DEVICE,0x2E,0x40);
+//8. Write 0x40/0x20 to Register 0x2F to assign single shock or
+//  double shock interrupt, respectively, to the INT2 pin.
+ writeTo(DEVICE,0x2F,0X40);
+//9.  Write 0xEA to Register 0x38 to enable triggered mode
+//FIFO. If an interrupt is detected on the INT2 pin, the FIFO
+//records the trigger event acceleration with 10 samples
+//retained from before the trigger event.
+ writeTo(DEVICE,0x38,0XEA);
+//10. Write 0x08 to Register 0x2D to start the measurement.
+//  It is recommended that the POWER_CTL register be
+//  configured last.
+ writeTo(DEVICE,0x2D,0x08);
 }
-void tap(void){
-  //Clear the interrupts on the ADXL345
-  readRegister(INT_SOURCE, 1, values); 
-  readRegister(DATAX0, 6, values);
+//int16_t ax, ay, az;
+volatile  bool bam=false;
+//
+void biff(){
+    bam=true;
+        digitalWrite(led,!digitalRead(led));
 
-  //The ADXL345 gives 10-bit acceleration values, but they are stored as bytes (8-bits). To get the full value, two bytes must be combined for each axis.
-  //The X value is stored in values[0] and values[1].
-  x = ((int)values[1]<<8)|(int)values[0];
-  //The Y value is stored in values[2] and values[3].
-  y = ((int)values[3]<<8)|(int)values[2];
-  //The Z value is stored in values[4] and values[5].
-  z = ((int)values[5]<<8)|(int)values[4];
-  kapow = true;
+ // if((unsigned)buff[0] & 0x40) {
+  //   bams++;
+  //   bam=true;
+   //  Serial.println("Oow");
+ // }
+ 
 }
+
 
 void loop(){
-   char devid[1];
-  
-    enterSleep();
+//  enterSleep();
+//      accel.getAcceleration(&ax, &ay, &az);
 
-if(kapow){
-     
-     Serial.println("Bammo");
-     Serial.print(x);
-     Serial.print(',');
-     Serial.print(y);
-     Serial.print(',');
-     Serial.println(z);
-     kapow=false;   
-     detachInterrupt(0);
-    delay(50);
-    attachInterrupt(0, tap, RISING);
-} 
-    }
-  
-   
+if(bam) {
+    bam=false;
+     readFrom(DEVICE,0X30,1,buff);
 
-
-void writeRegister(char registerAddress, char value){
-  digitalWrite(CS, LOW);
-  SPI.transfer(registerAddress);
-  SPI.transfer(value);
-  digitalWrite(CS, HIGH);
-}
-
-
-void readRegister(char registerAddress, int numBytes, char * values){
-  char address = 0x80 | registerAddress;
-  if(numBytes > 1) address = address | 0x40;
-  
-  digitalWrite(CS, LOW);
-  SPI.transfer(address);
-  for(int i=0; i<numBytes; i++){
-    values[i] = SPI.transfer(0x00);
   }
-  digitalWrite(CS, HIGH);
 }
+
+
+
+
+
